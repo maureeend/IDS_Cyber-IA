d64226fbe643a152dd130c142d54fa33b61ac374
maureen <drouard.maureen2@gmail.com>
2025-03-13 15:54:20 +0100

    Update train_model

--- models/train_model.py
+++ models/train_model.py
@@ -76,7 +76,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 print("good 2")
 
 # modèle Random Forest
-rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model = RandomForestClassifier(n_estimators=300, random_state=42)
 rf_model.fit(X_train, y_train)
 print("Entraînement terminé !")
 
