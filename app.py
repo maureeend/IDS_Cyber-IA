dfc1e1e6832a6a12124e163a16a56264f58df2c4
Mo <151353727+maureeend@users.noreply.github.com>
2025-03-27 13:07:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,23 @@ def run_script(script_path):
         print(f"Error : {script_path} introuvable.")
 
 if __name__ == "__main__":
-    print("=== DÉMARRAGE DU SYSTÈME DE DÉTECTION D'INTRUSION ===")
+    title = '''
+ _   _                      _____ _     _      _     _ 
+| \ | |                    /  ___| |   (_)    | |   | |
+|  \| | ___ _   _ _ __ ___ \ `--.| |__  _  ___| | __| |
+| . ` |/ _ \ | | | '__/ _ \ `--. \ '_ \| |/ _ \ |/ _` |
+| |\  |  __/ |_| | | | (_) /\__/ / | | | |  __/ | (_| |
+\_| \_/\___|\__,_|_|  \___/\____/|_| |_|_|\___|_|\__,_|
+                                                       
+                                                       
+'''
+
+    print(title)
+    print("\nBienvenue sur NeuroShield.")
+    print("Ce projet a été réalisé dans le cadre d'un cours et a pour but éducatif.")
+    print("Il vise à illustrer les concepts de cybersécurité et d'intelligence artificielle appliqués à la détection d'intrusions.")
+    print("\n===  DÉMARRAGE DE LA SURVEILLANCE EN TEMPS RÉEL  ===")
+
     
     # 1. Capture du trafic réseau
     run_script(capture_script)
